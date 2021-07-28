f2f367fe5c42b2396fede6578b56d3f256e2fea0
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2021-07-28 16:10:31 -0600

    Add css styling and input field event handler

--- main.js
+++ main.js
@@ -9,12 +9,24 @@ var studyActiveImage = document.querySelector('.js-study-active-image');
 var studyInactiveImage = document.querySelector('.js-study-inactive-image');
 var exerciseActiveImage = document.querySelector('.js-exercise-active-image');
 var exerciseInactiveImage = document.querySelector('.js-exercise-inactive-image');
-
+var numberInput = document.querySelector('.js-input-number');
 
 //Event Listeners
 activityButtons.addEventListener('click', function(event) {
   changeColor(event);
 });
+
+numberInput.addEventListener('keydown', funtion() {
+  this.value = this.value.replace(/[e\+\-]/gi, "");
+});
+
+numberInput.addEventListener("keydown", function(event) {
+  if (invalidChars.includes(event.key)) {
+    event.preventDefault();
+  }
+});
+  // allowNumbersOnly(event)
+// };
 
 //Event Handlers
 function changeColor(event) {
@@ -35,3 +47,30 @@ function changeColor(event) {
     exerciseButton.classList.toggle('exercise-active-button');
   }
 };
+
+function allowNumbersOnly(event) {
+  var invalidChars = ["-", "e", "+", "E"];
+  if (invalidChars.includes(event.key)){
+    event.preventDefault();
+  }
+}
+
+
+// inputBox.addEventListener("input", function() {
+//   this.value = this.value.replace(/[e\+\-]/gi, "");
+// });
+
+// inputBox.addEventListener("keydown", function(e) {
+//   if (invalidChars.includes(e.key)) {
+//     e.preventDefault();
+//   }
+// });
+
+// ('.js-input-number').keyup(function () {
+//     if (!this.value.match(/[0-9]/)) {
+//         this.value = this.value.replace(/[^0-9]/g, '');
+//     }
+// });
+// if (!this.value.match(/[0-9]/)) {
+//     event.preventDefault();
+//     this.value = this.value.replace(/[^0-9]/g, '');
