7c298802ccbc489009dd8be6c2c26961e9aedb77
Juliana Fargnoli <fargnoli.jm@gmail.com>
2021-07-27 20:26:21 -0600

    Create query selectors, eventListener, and eventHandler to change button status

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
-class Activity {
-  constructor(category, description, minutes, seconds, completed, id) {
-    this.category = category;
-    this.description = description; 
-    this.minutes = minutes;
-    this.seconds = seconds;
-    this.completed = completed;
-    this.id = id;
+// Variables
+var studyButton = document.querySelector('.js-study-button');
+var meditateButton = document.querySelector('.js-meditate-button');
+var exerciseButton = document.querySelector('.js-exercise-button');
+var activityButtons = document.querySelector('.js-activity-buttons');
+var meditateActiveImage = document.querySelector('.js-meditate-active-image');
+var meditateInactiveImage = document.querySelector('.js-meditate-inactive-image');
+var studyActiveImage = document.querySelector('.js-study-active-image');
+var studyInactiveImage = document.querySelector('.js-study-inactive-image');
+var exerciseActiveImage = document.querySelector('.js-exercise-active-image');
+var exerciseInactiveImage = document.querySelector('.js-exercise-inactive-image');
+
+
+//Event Listeners
+activityButtons.addEventListener('click', function(event) {
+  changeColor(event);
+});
+
+//Event Handlers
+function changeColor(event) {
+  event.preventDefault()
+
+  if(event.target.parentNode.classList.contains('js-study-button')) {
+    studyActiveImage.classList.toggle('hidden');
+    studyInactiveImage.classList.toggle('hidden');
+    studyButton.classList.toggle('study-active-button');
+
   }
-
-  countDown() {
-
-  };
-
-  markComplete() {
-
-  };
-
-  saveToStorage() {
-
-  };
-  
-}
+  //on click, add remove classes defined in css that are specific to that activity category
+};
