fe374f645c3e37d1a3843c86000733f5575143c9
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2021-07-28 18:56:09 -0600

    Add event handlert to prevent invalid keys in minute and seconds field

--- main.js
+++ main.js
@@ -9,24 +9,22 @@ var studyActiveImage = document.querySelector('.js-study-active-image');
 var studyInactiveImage = document.querySelector('.js-study-inactive-image');
 var exerciseActiveImage = document.querySelector('.js-exercise-active-image');
 var exerciseInactiveImage = document.querySelector('.js-exercise-inactive-image');
-var numberInput = document.querySelector('.js-input-number');
+var numberInput = document.querySelectorAll('.js-input-number');
 
 //Event Listeners
 activityButtons.addEventListener('click', function(event) {
   changeColor(event);
 });
 
-numberInput.addEventListener('keydown', funtion() {
-  this.value = this.value.replace(/[e\+\-]/gi, "");
-});
-
-numberInput.addEventListener("keydown", function(event) {
-  if (invalidChars.includes(event.key)) {
-    event.preventDefault();
-  }
-});
-  // allowNumbersOnly(event)
-// };
+numberInput.forEach(addNumberListeners)
+function addNumberListeners(input) {
+  input.addEventListener("keydown", function(event) {
+    var invalidChars = ["-", "e", "+", "E"];
+    if (invalidChars.includes(event.key)) {
+      event.preventDefault();
+    }
+  })
+};
 
 //Event Handlers
 function changeColor(event) {
@@ -47,30 +45,3 @@ function changeColor(event) {
     exerciseButton.classList.toggle('exercise-active-button');
   }
 };
-
-function allowNumbersOnly(event) {
-  var invalidChars = ["-", "e", "+", "E"];
-  if (invalidChars.includes(event.key)){
-    event.preventDefault();
-  }
-}
-
-
-// inputBox.addEventListener("input", function() {
-//   this.value = this.value.replace(/[e\+\-]/gi, "");
-// });
-
-// inputBox.addEventListener("keydown", function(e) {
-//   if (invalidChars.includes(e.key)) {
-//     e.preventDefault();
-//   }
-// });
-
-// ('.js-input-number').keyup(function () {
-//     if (!this.value.match(/[0-9]/)) {
-//         this.value = this.value.replace(/[^0-9]/g, '');
-//     }
-// });
-// if (!this.value.match(/[0-9]/)) {
-//     event.preventDefault();
-//     this.value = this.value.replace(/[^0-9]/g, '');
